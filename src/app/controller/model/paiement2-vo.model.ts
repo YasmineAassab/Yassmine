a0fb6907e08005458c35a2ccc16ae4665541f78d
amine <amineeddamir.15@gmail.com>
2021-06-23 13:34:51 +0100

    add paiement2vo anad paiement tabs in declarationtvalist

---
+++ src/app/controller/model/paiement2-vo.model.ts
@@ -0,0 +1,9 @@
+export class Paiement2Vo {
+    public ref: string;
+    public societeis: string;
+    public anneeis: string;
+    public societetva: string;
+    public anneetva: string;
+    public mois: string;
+    public trim: string;
+}
