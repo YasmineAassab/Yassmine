ea396d454046c67585a8e57925fcdcfcf86e9f15
YasmineAassab <yasmine.aassab@edu.uca.ma>
2021-06-23 16:18:23 +0100

    Add paiement.create.component

--- src/app/view/admin/paiement/paiement-create/paiement-create.component.ts
+++ src/app/view/admin/paiement/paiement-create/paiement-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Paiement2Service} from '../../../../controller/service/paiement.service';
+import {Paiement2} from '../../../../controller/model/paiement2.model';
 
 @Component({
   selector: 'app-paiement-create',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaiementCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: Paiement2Service) { }
 
   ngOnInit(): void {
+  }
+
+  public hideCreateDialog() {
+    this.createDialog = false;
+    this.submitted = false;
+  }
+
+  get createDialog(): boolean {
+    return this.service.createDialog;
+  }
+
+  set createDialog(value: boolean) {
+    this.service.createDialog = value;
+  }
+
+  get submitted(): boolean {
+    return this.service.submitted;
+  }
+
+  set submitted(value: boolean) {
+    this.service.submitted = value;
+  }
+
+  get items(): Array<Paiement2> {
+    return this.service.items;
+  }
+
+  set items(value: Array<Paiement2>) {
+    this.service.items = value;
+  }
+
+  get selected(): Paiement2 {
+    return this.service.selected;
+  }
+
+  set selected(value: Paiement2) {
+    this.service.selected = value;
   }
 
 }
